962fb8b79754f4ae529deb872ddcb3db3c52a547
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2023-08-29 00:06:49 +0530

    Time: 295 ms (98.41%), Space: 160.9 MB (71.77%) - LeetHub

--- 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.cpp
@@ -11,34 +11,16 @@
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
-        vector<int> arr;
-        ListNode *p=head,*q=nullptr;
-        while(p){
-            arr.push_back(p->val); 
-            p=p->next;
-        }
-        stack<pair<int,int>> st; 
-        for(int i=arr.size()-1;i>=0;i--){
-            if(st.empty()){
-                st.push({arr[i],1}); 
-            }else{
-                if(st.top().first>arr[i]){ 
-                    st.push({st.top().first,-1});
-                }else{
-                    st.push({arr[i],1}); 
-                }
-            }
-        }
-        p=head;
-        while(st.size()){
-            if(st.top().second==1){
-                p->val = st.top().first;
-                q=p; 
-                p=p->next;
-            }
-            st.pop();
-        }
-        q->next = nullptr;
+        if(head == NULL || head->next == NULL)
+            return head;
+
+        ListNode *newNode = removeNodes(head->next);
+
+        if(newNode->val > head->val)
+            return newNode;
+
+        head->next = newNode;
+
         return head;
     }
 };
