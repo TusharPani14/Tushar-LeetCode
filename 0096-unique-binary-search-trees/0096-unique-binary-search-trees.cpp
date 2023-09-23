d4f21af8fc447dab6ecbcd853d6f03cd568f4e1b
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2023-09-23 10:55:43 +0530

    Time: 2 ms (61.27%), Space: 6.4 MB (5.44%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int numTrees(int n) {
-        if(n==0 || n==1){
-            return 1;
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
         }
-        int sum=0;
-        for(int i=1;i<=n;i++){
-            sum+=numTrees(i-1)*numTrees(n-i);
-        }
-        return sum;
+
+        return dp[n];
     }
 };
