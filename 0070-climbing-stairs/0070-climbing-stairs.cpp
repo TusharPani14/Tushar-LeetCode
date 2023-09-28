ed0bdf19c171a0be136a05dcc3f3c37bda82b110
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2023-09-29 01:45:11 +0530

    Time: 0 ms (100%), Space: 6.6 MB (6.54%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int solve(int n,vector<int> &dp){
+        if(n==1 || n==2){
+            return n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
