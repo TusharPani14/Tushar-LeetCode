8cb1ba78216dbfdcaba60dbc5463080a5af4dec5
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2023-10-03 01:53:09 +0530

    Time: 20 ms (90.39%), Space: 6.7 MB (85.57%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -18,7 +18,25 @@ public:
         }
     }
     int numDistinct(string s, string t) {
-        vector<vector<int>>dp(s.length(),vector<int>(t.length(),-1));
-        return solve(s, t, s.length()-1,t.length()-1,dp);
+        // vector<vector<int>>dp(s.length(),vector<int>(t.length(),-1));
+        // return solve(s, t, s.length()-1,t.length()-1,dp);
+        int n = s.size();
+		int m = t.size();
+		int mod = 1e9+7;
+        vector<int> dp(m+1,0);
+
+		dp[0] = 1;
+        for(int i = 1; i<=n ; i++)
+		{
+			for(int j = m ; j>=1 ; j--)
+			{
+				if(s[i-1] == t[j-1])
+				{
+					dp[j] = (dp[j-1] + dp[j]) % mod;
+				}
+			}
+		}
+
+		return dp[m];   
     }
 };
