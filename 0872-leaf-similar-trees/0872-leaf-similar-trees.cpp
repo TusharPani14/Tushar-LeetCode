7d5e18b6486c5bb74d353423120054a5a1c1dad7
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2024-01-09 18:16:34 +0530

    Time: 7 ms (20.29%), Space: 13.5 MB (13.67%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -6,26 +6,27 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void solve(TreeNode* &root, vector<int> &leaf){
-        if(root==NULL){
+    void solve(TreeNode*& root, vector<int>& leaf) {
+        if (root == NULL) {
             return;
         }
-        if(root->left==NULL && root->right==NULL){
+        if (root->left == NULL && root->right == NULL) {
             leaf.push_back(root->val);
         }
-        solve(root->left,leaf);
-        solve(root->right,leaf);
+        solve(root->left, leaf);
+        solve(root->right, leaf);
     }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int>leaf1,leaf2;
+        vector<int> leaf1, leaf2;
         solve(root1, leaf1);
         solve(root2, leaf2);
-        if(leaf1==leaf2){
+        if (leaf1 == leaf2) {
             return true;
         }
         return false;
