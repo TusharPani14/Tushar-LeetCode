9e44d0815f8b48e5417bba817f395f11236ee8f5
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2024-06-21 18:32:23 +0530

    Time: 0 ms (100%), Space: 42.3 MB (43.79%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,24 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        return calculate(x, n);
+    }
+
+    public double calculate(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        double half = calculate(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+}
