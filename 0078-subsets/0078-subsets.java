ae0bfa9d41244043803a5979b734676d72e44ac1
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2024-06-24 23:58:08 +0530

    Time: 1 ms (52.34%), Space: 42.4 MB (84.53%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -7,13 +7,13 @@ class Solution {
     }
 
     public void solve(List<List<Integer>> l, List<Integer> sl, int[] nums, int i) {
-        if (i >= nums.length) {   
+        if (i >= nums.length) {
             l.add(new ArrayList<>(sl));
             return;
         }
         sl.add(nums[i]);
-        solve(l,sl,nums,i+1);
-        sl.remove(sl.size()-1);
-        solve(l,sl,nums,i+1);
+        solve(l, sl, nums, i + 1);
+        sl.remove(sl.size() - 1);
+        solve(l, sl, nums, i + 1);
     }
 }
