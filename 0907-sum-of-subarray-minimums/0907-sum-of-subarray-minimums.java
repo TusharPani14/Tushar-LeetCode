25381be45f35a08cb801f4f03c8c6cbba6aec2b4
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2024-06-25 12:15:27 +0530

    Time: 63 ms (40.85%), Space: 49.9 MB (62.74%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,30 +1,25 @@
 class Solution {
     public int sumSubarrayMins(int[] A) {
-      // initialize previous less element and next less element of 
-      // each element in the array
-        
         Stack<int[]> previousLess = new Stack<>();
         Stack<int[]> nextLess = new Stack<>();
         int[] leftDistance = new int[A.length];
         int[] rightDistance = new int[A.length];
         
-        for(int i=0; i<A.length; i++)
-        {
+        // Calculate distances to the previous less elements
+        for (int i = 0; i < A.length; i++) {
             // use ">=" to deal with duplicate elements
-            while(!previousLess.isEmpty() && previousLess.peek()[0] >= A[i])
-            {
+            while (!previousLess.isEmpty() && previousLess.peek()[0] > A[i]) {
                 previousLess.pop();
             }
             
-            leftDistance[i] = previousLess.isEmpty() ? i+1 : i - previousLess.peek()[1];
+            leftDistance[i] = previousLess.isEmpty() ? i + 1 : i - previousLess.peek()[1];
             previousLess.push(new int[]{A[i], i});
-            
         }
         
-        for(int i=A.length-1; i>=0; i--)
-        {
-            while(!nextLess.isEmpty() && nextLess.peek()[0] > A[i])
-            {
+        // Calculate distances to the next less elements
+        for (int i = A.length - 1; i >= 0; i--) {
+            // use ">" to deal with duplicate elements
+            while (!nextLess.isEmpty() && nextLess.peek()[0] >= A[i]) {
                 nextLess.pop();
             }
             
@@ -32,11 +27,12 @@ class Solution {
             nextLess.push(new int[]{A[i], i});
         } 
         
+        // Calculate the result using left and right distances
         int ans = 0;
         int mod = 1000000007;
-        for(int i=0; i<A.length; i++)
-        {
-            ans = (ans + A[i] * leftDistance[i] * rightDistance[i]) % mod;
+        for (int i = 0; i < A.length; i++) {
+            long product = (long) A[i] * leftDistance[i] * rightDistance[i];
+            ans = (int) ((ans + product) % mod);
         }
         return ans;
     }
