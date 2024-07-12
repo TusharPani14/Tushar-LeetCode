e24572a3e890d8c16454e709d088d41ae783f7c0
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2024-07-12 23:40:04 +0530

    Time: 3 ms (53.6%), Space: 45.4 MB (59.78%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,27 +1,35 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        Boolean[] memo = new Boolean[nums.length];
-        return solve(nums, 0, memo);
-    }
-    
-    private boolean solve(int[] nums, int i, Boolean[] memo) {
-        if (i >= nums.length - 1) {
-            return true; 
-        }
-        
-        if (memo[i] != null) {
-            return memo[i];
-        }
-        
-        int furthestJump = Math.min(i + nums[i], nums.length - 1);
-        for (int j = i + 1; j <= furthestJump; j++) {
-            if (solve(nums, j, memo)) {
-                memo[i] = true;
-                return true;
+        // Boolean[] memo = new Boolean[nums.length];
+        // return solve(nums, 0, memo);
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > max) {
+                return false;
             }
+            max = Math.max(max, i + nums[i]);
         }
-        
-        memo[i] = false;
-        return false;
+        return true;
     }
+
+    // private boolean solve(int[] nums, int i, Boolean[] memo) {
+    // if (i >= nums.length - 1) {
+    // return true;
+    // }
+
+    // if (memo[i] != null) {
+    // return memo[i];
+    // }
+
+    // int furthestJump = Math.min(i + nums[i], nums.length - 1);
+    // for (int j = i + 1; j <= furthestJump; j++) {
+    // if (solve(nums, j, memo)) {
+    // memo[i] = true;
+    // return true;
+    // }
+    // }
+
+    // memo[i] = false;
+    // return false;
+    // }
 }
