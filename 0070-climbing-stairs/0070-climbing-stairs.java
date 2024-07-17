988fb190319d2ef7e98701f8f9aa4319aed36650
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2024-07-17 09:05:23 +0530

    Time: 0 ms (100%), Space: 39.9 MB (84.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -6,13 +6,33 @@ class Solution {
         return solve(n, dp);
     }
 
-    public int solve(int n, int[] dp) {
-        if (n == 0 || n == 1) {
-            return 1;
+    // public int solve(int n, int[] dp) {
+    //     if (n == 0 || n == 1) {
+    //         return 1;
+    //     }
+    //     if (dp[n] != -1) {
+    //         return dp[n];
+    //     }
+    //     return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+    // }
+
+    // public int solve(int n, int[]dp){
+    //     dp[0]=1;
+    //     dp[1]=1;
+    //     for(int i=2;i<=n;i++){
+    //         dp[i]=dp[i-1]+dp[i-2];
+    //     }
+    //     return dp[n];
+    // }
+
+    public int solve(int n, int[]dp){
+        int prev=1;
+        int prev2=1;
+        for(int i=2;i<=n;i++){
+            int cur=prev+prev2;
+            prev2=prev;
+            prev=cur;
         }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+        return prev;
     }
 }
