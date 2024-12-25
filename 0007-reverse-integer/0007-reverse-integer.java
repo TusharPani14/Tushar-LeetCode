939da7faeb50e75e8c42b3bb295f30ca9a79285f
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2024-12-25 21:36:53 +0530

    Time: 1 ms (85.5%), Space: 41.3 MB (11.71%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -3,6 +3,9 @@ class Solution {
         int rev=0;
         while(x>0){
             int d=x%10;
+            if(rev>(Integer.MAX_VALUE-d)/10){
+                return 0;
+            }
             rev=rev*10+d;
             x=x/10;
         }
