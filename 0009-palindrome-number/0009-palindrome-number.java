e010e4598ae58ae185a120a0be9cb52526b549f9
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2025-07-16 23:30:34 +0530

    Time: 4 ms (100%), Space: 44.2 MB (36.99%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,15 +1,15 @@
 class Solution {
-    public int helper(int x){
-        int res=0;
-        while(x>0){
-            int d=x%10;
-            res=res*10+d;
-            x=x/10;
-        }
-        return res;
-    }
     public boolean isPalindrome(int x) {
-        if(x>=0 && x==helper(x)){
+        if(x<0){
+            return false;
+        }
+        int m=x,d=0,res=0;
+        while(m>0){
+            d=m%10;
+            res=res*10+d;
+            m=m/10;
+        }
+        if(res==x){
             return true;
         }
         return false;
