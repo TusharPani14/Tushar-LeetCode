185742cb3daf546dee003ff70a07893a6fec1883
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2025-07-22 00:37:16 +0530

    Time: 0 ms (100%), Space: 44.6 MB (96.24%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
+    // Better sol using hashmap
     // public int removeDuplicates(int[] nums) {
-    //     //Better sol using hashmap
     //     int c=0;
     //     TreeSet<Integer> set=new TreeSet<>();
     //     for(int i=0;i<nums.length;i++){
@@ -12,6 +12,7 @@ class Solution {
     //     }
     //     return c;
     // }
+    // Optimal approach
     public int removeDuplicates(int[] nums){
         int i=0;
         for(int j=1;j<nums.length;j++){
