111d5ded43c015907bb0930f9394196401ad7236
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2025-07-25 00:48:14 +0530

    Time: 1 ms (99.86%), Space: 53.2 MB (48.88%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int count = 0;
-        int candidate = 0;
-        
-        for (int num : nums) {
-            if (count == 0) {
-                candidate = num;
-            }
-            
-            if (num == candidate) {
-                count++;
-            } else {
-                count--;
+        //Moore’s Voting Algorithm
+        int c=0;
+        int elem=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(c==0){
+                elem=nums[i];
+                c++;
+            }else{
+                if(elem==nums[i]){
+                    c++;
+                }else{
+                    c--;
+                }
             }
         }
-        
-        return candidate;
+        return elem;
     }
 }
