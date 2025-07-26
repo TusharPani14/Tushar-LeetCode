db348a4d43c91af0a3a6dcc41545445bdc29ed9a
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2025-07-26 17:56:26 +0530

    Time: 24 ms (72.44%), Space: 46.4 MB (65.55%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,45 +1,16 @@
 class Solution {
-    // Sliding Window approach is for +ve numbers
-    // public int subarraySum(int[] nums, int k) {
-    //     int j = 0, sum = 0, ans = 0;
-    //     if (nums.length == 1) {
-    //         if (nums[0] == k) {
-    //             return 1;
-    //         } else
-    //             return 0;
-    //     }
-    //     for (int i = 0; i < nums.length; i++) {
-    //         sum += nums[i];
-    //         while (sum > k) {
-    //             sum = sum - nums[j];
-    //             j++;
-    //         }
-    //         if (sum == k) {
-    //             ans++;
-    //         }
-    //     }
-    //     return ans;
-    // }
-
-    // To consider -ve numbers we have to use prefix sum
     public int subarraySum(int[] nums, int k) {
-        int n=nums.length;
-        int psum[] = new int[n];
-        int ans=0;
-        HashMap<Integer,Integer> mp= new HashMap<>();
-        psum[0]=nums[0];
-        for(int i=1;i<n;i++){
-            psum[i]=psum[i-1]+nums[i];
-        }
-        for(int i=0;i<n;i++){
-            if(psum[i]==k){
-                ans++;
+        //prefix sum and hashmap for storing count
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        int sum=0,c=0;
+        mp.put(0, 1);
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(mp.containsKey(sum-k)){
+                c+=mp.get(sum-k);
             }
-            if(mp.containsKey(psum[i]-k)){
-                ans+=mp.get(psum[i]-k);
-            }
-            mp.put(psum[i],mp.getOrDefault(psum[i], 0) + 1);
+            mp.put(sum,mp.getOrDefault(sum,0)+1);
         }
-        return ans;
+        return c;
     }
 }
