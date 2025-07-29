0c05d11630ff5f7796d3efd01401da28c5ebdd16
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2025-07-30 00:53:04 +0530

    Time: 3 ms (76.5%), Space: 50.8 MB (9.38%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,17 +1,41 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length;
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (mp.getOrDefault(nums[i], 0) != -1) {
-                mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
+        //At max two elems can be majority elem
+        //We will apply the modified voting algo that we have applied in Maj Elem1
+        int elem1=Integer.MIN_VALUE;
+        int elem2=Integer.MIN_VALUE;
+        int c1=0,c2=0;
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if((elem1==nums[i]||c1==0) && elem2!=nums[i]){
+                elem1=nums[i];
+                c1++;
             }
-            if (mp.get(nums[i]) > Math.floor(n / 3)) {
-                res.add(nums[i]);
-                mp.put(nums[i], -1);
+            else if((elem2==nums[i]||c2==0) && elem1!=nums[i]){
+                elem2=nums[i];
+                c2++;
+            }
+            else{
+                c1--;
+                c2--;
             }
         }
-        return res;
+        c1=0;
+        c2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==elem1){
+                c1++;
+            }
+            if(nums[i]==elem2){
+                c2++;
+            }
+        }
+        if(c1>nums.length/3){
+            ans.add(elem1);
+        }
+        if(c2>nums.length/3){
+            ans.add(elem2);
+        }
+        return ans;
     }
 }
