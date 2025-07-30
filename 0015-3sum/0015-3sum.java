3e854c0834ee03a21a5c2d69861368f857e09d56
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2025-07-30 23:14:47 +0530

    Time: 45 ms (24.91%), Space: 52.9 MB (13.39%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -6,25 +6,63 @@ class Solution {
         //So by this method we can optimise it to N^2
         //But one catch is there that we cannot put all elems at once in hashset and look for nums[k] because it may find the same number where i or j are standing. For detailed explanation look notes
         //So we have to take hashset elems between i and j
+        // List<List<Integer>> ans=new ArrayList<>();
+        // HashSet<List<Integer>> uniqueSet=new HashSet<>();
+        // for(int i=0;i<nums.length;i++){
+        //     HashSet<Integer> set=new HashSet<>();
+        //     for(int j=i+1;j<nums.length;j++){
+        //         List<Integer> row=new ArrayList<>();
+        //         int third=-(nums[i]+nums[j]);
+        //         if(set.contains(third)){
+        //             row.add(nums[i]);
+        //             row.add(nums[j]);
+        //             row.add(third);
+        //             Collections.sort(row);
+        //             uniqueSet.add(row);
+        //         }
+        //         set.add(nums[j]);
+        //     }
+        // }
+        // for(var s: uniqueSet){
+        //     ans.add(s);
+        // }
+        // return ans;
+
+
+        //Now optimal approach
+        //sort
+        //Fix move i and k from both ends
+        //-4,-1,-1,0,1,2
+        Arrays.sort(nums);
         List<List<Integer>> ans=new ArrayList<>();
-        HashSet<List<Integer>> uniqueSet=new HashSet<>();
         for(int i=0;i<nums.length;i++){
-            HashSet<Integer> set=new HashSet<>();
-            for(int j=i+1;j<nums.length;j++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
                 List<Integer> row=new ArrayList<>();
-                int third=-(nums[i]+nums[j]);
-                if(set.contains(third)){
+                if(nums[i]+nums[j]+nums[k]==0){
                     row.add(nums[i]);
                     row.add(nums[j]);
-                    row.add(third);
-                    Collections.sort(row);
-                    uniqueSet.add(row);
+                    row.add(nums[k]);
+                    ans.add(row);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]){
+                        j++;
+                    }
+                    while(j<k && nums[k]==nums[k+1]){
+                        k--;
+                    }
                 }
-                set.add(nums[j]);
+                else if(nums[i]+nums[j]+nums[k]<0){
+                    j++;
+                }else{
+                    k--;
+                }
             }
-        }
-        for(var s: uniqueSet){
-            ans.add(s);
         }
         return ans;
     }
