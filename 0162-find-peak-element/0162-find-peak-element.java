ffb36d9917a5e5c1d4824291c695f71141093fc6
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2025-08-07 00:25:21 +0530

    Time: 0 ms (100%), Space: 42.6 MB (31.41%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,26 +1,27 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int n=nums.length;
-        if(n==1){
+        //check if mid is greater than its both side. If yes return mid;
+        //if increasing slope assume the peak will not be there remove left half
+        //else remove right half(if it is decreasing slope of else it may be bottom or valley)
+        int n = nums.length;
+        if (n == 1) {
             return 0;
         }
-        if(nums[0]>nums[1]){
+        if (nums[0] > nums[1]) {
             return 0;
         }
-        if(nums[n-2]<nums[n-1]){
-            return n-1;
+        if (nums[n - 2] < nums[n - 1]) {
+            return n - 1;
         }
-        int low=1,high=n-2;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+        int low = 1, high = n - 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
                 return mid;
-            }
-            else if(nums[mid-1]<=nums[mid] && nums[mid]<=nums[mid+1]){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
+            } else if (nums[mid - 1] <= nums[mid] && nums[mid] <= nums[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
         return -1;
