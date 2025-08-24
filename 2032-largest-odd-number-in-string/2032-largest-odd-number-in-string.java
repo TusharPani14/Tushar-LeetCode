e81196196c836de9993c3857adffea5af20c69ea
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2025-08-25 01:44:12 +0530

    Time: 1 ms (99.48%), Space: 45.7 MB (13.26%) - LeetHub

---
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        for(int i=num.length()-1;i>=0;i--){
+            char ch=num.charAt(i);
+            if(ch%2!=0){
+                return num.substring(0,i+1);
+            }
+        }
+        return "";
+    }
+}
