c7ce7474daaee9fcfa7bcb4e383707b53f7b04ff
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2025-08-27 00:50:00 +0530

    Time: 1 ms (100%), Space: 42.8 MB (15.07%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,43 +1,28 @@
 class Solution {
     public int myAtoi(String s) {
-        boolean nve = false, digit = false;
-        long res = 0;
+        s = s.trim();
+        int sign = 1;
+        int num = 0;
+        int res = 0;
+        if (s.length() == 0) {
+            return 0;
+        }
+        if (s.charAt(0) == '-') {
+            sign = -1;
+        }
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == ' ') {
-                if (digit)
-                    break;
-                continue;
-            } else if (ch == '-') {
-                if (digit)
-                    break;
-                nve = true;
-                digit = true;
-            } else if (ch == '+') {
-                if (digit)
-                    break;
-                digit = true;
-            } else if (Character.isDigit(ch)) {
-                digit = true;
-                res = res * 10 + (ch - '0');
-                if (res > Integer.MAX_VALUE) {
-                    break;
+            if (Character.isDigit(s.charAt(i))) {
+                int digit = s.charAt(i) - '0';
+                if (res > (Integer.MAX_VALUE - digit) / 10) {
+                    return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
                 }
-            } else {
+                res = res * 10 + digit;
+            } else if (i == 0 && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+                continue;
+            } else if (!Character.isDigit(s.charAt(i))) {
                 break;
             }
         }
-
-        if (nve) {
-            res = -res;
-        }
-
-        if (res < Integer.MIN_VALUE) {
-            return Integer.MIN_VALUE;
-        } else if (res > Integer.MAX_VALUE) {
-            return Integer.MAX_VALUE;
-        }
-
-        return (int) res;
+        return res * sign;
     }
 }
