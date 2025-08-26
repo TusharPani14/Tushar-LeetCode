7da7908617850e043c21112cffe66cc63fa04fba
Tushar Panigrahi <97904669+TusharPani14@users.noreply.github.com>
2025-08-26 23:46:27 +0530

    Time: 0 ms (100%), Space: 41.3 MB (93.05%) - LeetHub

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxDepth(String s) {
+        int c=0,max=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                c++;
+                max=Math.max(max,c);
+            }else if(ch==')'){
+                c--;
+            }
+        }
+        return max;
+    }
+}
